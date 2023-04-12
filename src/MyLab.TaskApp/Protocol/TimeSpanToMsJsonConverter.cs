24dd2d4db3ff1a39c3ce022125a5f4f21407bb3e
Andrey O <ozzy.ext@gmail.com>
2023-04-12 14:23:24 +0300

    Rounded millisecond duration in protocol

---
+++ src/MyLab.TaskApp/Protocol/TimeSpanToMsJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyLab.TaskApp.Protocol
+{
+    class TimeSpanToMsJsonConverter : JsonConverter<TimeSpan>
+    {
+        public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Math.Round(value.TotalMilliseconds).ToString("F0"));
+        }
+
+        public override TimeSpan ReadJson(JsonReader reader, Type objectType, TimeSpan existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
